852027f2732062d6b5a1ed833428d44ef2e9637f
Dmitri Kuznetsov <kna_int@mail.ru>
2022-06-30 21:35:16 +0300

    Updated lang and .net version

--- Godot/scripts/script1.cs
+++ Godot/scripts/script1.cs
@@ -10,7 +10,7 @@ public class script1 : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        GD.Print("Executed C# code !");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
